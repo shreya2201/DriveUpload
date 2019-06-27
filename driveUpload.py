bb9d5a8e8b4e04b453e296f75a44929b35470ad8
shreya2201 <shreyapohekar@gmail.com>
2019-06-27 23:40:16 +0530

    Update driveUpload.py
    tracking of uploaded files to remove any duplicate uploads.

--- driveUpload.py
+++ driveUpload.py
@@ -23,26 +23,41 @@ gauth.SaveCredentialsFile("mycreds.txt")
 
 drive = GoogleDrive(gauth)
 
-#file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
-#for file1 in file_list:       
-#    print ('title: %s, id: %s' % (file1['title'], file1['id']))
-
-
-
+#fid is the id of the folder in drive where all the files will be backed up
 fid = '1iqirhS_XUUsDQFq4z0Xc6KhBSfznhTCY'
 
 #insert_permission(drive,fid,None,'user','owner')
 
+fileobject = open('tracking.txt','r')    #tracking.txt keeps the track of the files that are being uploaded to the drive
+file_list = fileobject.readlines()
+print(file_list)
+print(file_list[0])
+present = False
 import glob, os
-os.chdir("/home/shreya/Documents")
-for file in glob.glob("plot.png"):
+os.chdir("/home/shreya/Documents")          #it is the directory that is being backed up on google drive
+for file in glob.glob("emp.txt"):
     print(file)
     with open(file,"r") as f:
         fn = os.path.basename(f.name)
-        file_drive = drive.CreateFile({"parents": [{"kind": "drive#fileLink", "id": fid}]})
-    file_drive.SetContentFile(fn) 
-    
-    file_drive.Upload()
-    print ("The file: " + fn + " has been uploaded")
-   
+        for i in range(len(file_list)):
+            print(i)
+            # this removes the extra \n that is present after every file name
+            file_list[i] = file_list[i].replace("\n","").strip()  
+            if(file_list[i] == fn):
+                present = True
+                print("match found for ",fn)
+                break
+ #if the file name is not present tracking.txt, it will upload that file to drive alongside making an entry in tracking.txt                
+        if(present == False):           
+            file_drive = drive.CreateFile({"parents": [{"kind": "drive#fileLink", "id": fid}]})
+            file_drive.SetContentFile(fn) 
+            file_drive.Upload()
+            print ("The file: " + fn + " has been uploaded")
+            fileWriteObject = open('tracking.txt','a')
+            s = fn
+            fileWriteObject.write(s+"\n")
+            print("the file is appended to tracking.txt")
+
+fileobject.close()
+fileWriteObject.close()          
 print ("All files have been uploaded")
